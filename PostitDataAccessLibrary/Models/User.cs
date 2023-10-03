32a48dd17812cb69d342d5ce6884ba01a2b5a1ce
Azizcan Yalcin <202003001014@dogus.edu.tr>
2023-10-03 10:15:33 +0300

    Add project files.

---
+++ PostitDataAccessLibrary/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostitDataAccessLibrary.Models
+{
+    public class User
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Password { get; set; }
+        Email EmailAdress { get; set; } = new Email();
+        DateTime Birthday { get; set; }
+
+        
+    }
+}
